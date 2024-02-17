2b12531a504f5567e5ad2881bc6b5be48462f1a9
Israel Galvan <israel.galvan@live.com>
2024-02-17 09:07:58 -0300

    IbaseRepositorie definition

--- src/Core/Blog.Domain/Interfaces/Repositories/IBaseRepositorie.cs
+++ src/Core/Blog.Domain/Interfaces/Repositories/IBaseRepositorie.cs
@@ -1,6 +1,13 @@
-﻿namespace Blog.Domain;
+﻿using Blog.Domain.Entities;
 
-public interface IBaseRepositorie
+namespace Blog.Domain.Interfaces.Repositories;
+
+public interface IBaseRepositorie<T> where T : BaseEntity
 {
-
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<T> Get(Guid id);
+    Task<IEnumerable<T>> GetAll();
+    Task Commit();
 }
