cb718373df9272639d5347bd1cf439da050532c2
Israel Galvan <israel.galvan@live.com>
2024-02-17 11:15:03 -0300

    Context created

---
+++ src/Infrastructure/Blog.Infrastructure.Persistance/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure.Persistance;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : DbContext(options)
+{
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
