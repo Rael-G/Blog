02d69a79f3a8260310f2b407c71b07a62c1c7608
Israel Galvan <israel.galvan@live.com>
2024-02-17 11:26:46 -0300

    HostExtensions for Infrastructure created

---
+++ src/Infrastructure/Blog.Infrastructure.Persistance/HostExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Blog.Domain;
+using Blog.Domain.Entities;
+
+
+namespace Blog.Infrastructure.Persistance;
+
+public static class HostExtensions
+{
+    public static void ConfigurePersistance(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql
+            (configuration.GetConnectionString("Postgres")));
+
+        services.AddScoped<IPostRepository, PostRepository>();
+        services.AddScoped<ICommentRepository, CommentRepository>();
+    }
+}
