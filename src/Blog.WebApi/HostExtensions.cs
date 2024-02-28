72f8aa7775dcf74660ca4dfaf107d10a1f558498
Israel Albino Galvan <israel.galvan@live.com>
2024-02-28 18:56:26 -0300

    Migrations

---
+++ src/Blog.WebApi/HostExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.WebApi
+{
+    public static class HostExtensions
+    {
+        public static void InitializeDb(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<ApplicationDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+                context.Database.Migrate();
+        }
+    }
+}
