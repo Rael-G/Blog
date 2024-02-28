284afbd2c374aa68c98773d4f712704115c4d116
Israel Albino Galvan <israel.galvan@live.com>
2024-02-28 15:31:01 -0300

    PostsController and InputModel

---
+++ src/Blog.WebApi/Controllers/PostsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Blog.Application;
+using Blog.WebApi.Models.Input;
+
+namespace Blog.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController(IPostService postService) 
+        : ControllerBase
+    {
+        private readonly IPostService _postService = postService;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+            => Ok(await _postService.GetAll());
+        
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var post = await _postService.Get(id);
+
+            if (post is null)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PostInputModel input)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var post = input.InputToDto();
+            try
+            {
+                _postService.Create(post);
+            }
+            catch(ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            await _postService.Commit();
+            return CreatedAtAction(nameof(Get), new { post.Id }, post);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] PostInputModel input)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var existentPost = await _postService.Get(id);
+            if (existentPost is null)
+                return NotFound();
+
+            var post = input.InputToDto(id);
+            try
+            {
+                _postService.Update(post);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            await _postService.Commit();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var post = await _postService.Get(id);
+
+            if (post is null)
+                return NotFound();
+
+            _postService.Delete(post);
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/Blog.WebApi/Models/Input/PostInputModel.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Application;
+using Blog.Domain;
+
+namespace Blog.WebApi.Models.Input
+{
+    public record PostInputModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+
+        public PostDto InputToDto(Guid? id = null)
+            => new() 
+            {
+                Id = id ?? Guid.NewGuid(),
+                Title = Title, 
+                Content = Content,
+                Comments = Comments
+            };
+    }
+}
