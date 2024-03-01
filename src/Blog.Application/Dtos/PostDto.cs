96305d2c71232547f046ed6da1d73ccdfd58fdde
Israel Albino Galvan <israel.galvan@live.com>
2024-03-01 14:49:44 -0300

    PostDto now has many CommentDtos instead of Comments

--- src/Blog.Application/Dtos/PostDto.cs
+++ src/Blog.Application/Dtos/PostDto.cs
@@ -1,19 +1,17 @@
-﻿using Blog.Domain;
-
-namespace Blog.Application;
+﻿namespace Blog.Application;
 
 public class PostDto
 {
     public Guid? Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    public IEnumerable<Comment> Comments { get; set; }
+    public IEnumerable<CommentDto> Comments { get; set; }
 
-    public PostDto(string title, string content, IEnumerable<Comment>? comments = null)
+    public PostDto(string title, string content, IEnumerable<CommentDto> comments)
     {
         Title = title;
         Content = content;
-        Comments = comments ?? [];
+        Comments = comments;
     }
 
     public PostDto()
