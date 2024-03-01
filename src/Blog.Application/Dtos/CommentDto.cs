a061869a28cab5f13c8b99efe12992545a8385e0
Israel Albino Galvan <israel.galvan@live.com>
2024-03-01 14:13:26 -0300

    Fitting new domain structure

--- src/Blog.Application/Dtos/CommentDto.cs
+++ src/Blog.Application/Dtos/CommentDto.cs
@@ -2,14 +2,16 @@
 
 public class CommentDto
 {
-    public Guid Id { get; set; }
+    public Guid? Id { get; set; }
     public string Author { get; set; }
     public string Content { get; set; }
+    public Guid PostId { get; set; }
 
-    public CommentDto(string author, string content)
+    public CommentDto(string author, string content, Guid postId)
     {
         Author = author;
         Content = content;
+        PostId = postId;
     }
 
     public CommentDto()
