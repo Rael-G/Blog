dc3b1a973ac5b10303fc658d010c9144d9487eea
Israel Albino Galvan <israel.galvan@live.com>
2024-03-01 14:02:06 -0300

    Post and PostId for Comment and FluentApi

--- src/Blog.Persistance/Contexts/ApplicationDbContext.cs
+++ src/Blog.Persistance/Contexts/ApplicationDbContext.cs
@@ -8,4 +8,24 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<Post> Posts { get; set; }
     public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Post>(post =>
+        {
+            post.HasKey(p => p.Id);
+            post.Property(p => p.Title).IsRequired().HasMaxLength(256);
+            post.Property(p => p.Content).IsRequired();
+            post.HasMany(p => p.Comments).WithOne(c => c.Post).HasForeignKey(c => c.PostId);
+        });
+
+        modelBuilder.Entity<Comment>(comment =>
+        {
+            comment.HasKey(c => c.Id);
+            comment.Property(c => c.Author).IsRequired().HasMaxLength(256);
+            comment.Property(c => c.Content).IsRequired().HasMaxLength(512);
+        });
+    }
 }
---
+++ src/Blog.Persistance/Migrations/20240229191720_CommentBelongsToPost.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentBelongsToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Comments",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Comments",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Comments",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(512)",
+                oldMaxLength: 512);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Comments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+    }
+}
