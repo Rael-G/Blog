d57f6acdc4c8db481f0a8fa4bbf7470b0f8058b4
Israel Albino Galvan <israel.galvan@live.com>
2024-03-14 16:13:12 -0300

    Management and create-post pages

--- src/Blog.WebUi/src/app/app.routes.ts
+++ src/Blog.WebUi/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { PostComponent } from './components/pages/post/post.component';
+import { ManagementComponent } from './components/pages/management/management.component';
+import { CreatePostComponent } from './components/pages/create-post/create-post.component';
 
 export const routes: Routes = 
 [
     {path: '', component:HomeComponent},
     {path: 'about', component:AboutComponent},
     {path: 'contact', component:ContactComponent},
-    {path: 'posts/example', component:PostComponent}
+    {path: 'posts/example', component:PostComponent},
+    {path: 'management', component:ManagementComponent},
+    {path: 'create-post', component:CreatePostComponent}
+
 ];
---
+++ src/Blog.WebUi/src/app/components/pages/management/management.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-management',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './management.component.html',
+  styleUrl: './management.component.scss'
+})
+export class ManagementComponent {
+  lorem: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, enim vel sodales volutpat, lorem ipsum aliquet velit, ut hendrerit nulla odio ut dolor. Quisque vulputate risus id lacus commodo, et rutrum arcu lacinia. Phasellus hendrerit diam at leo congue sollicitudin.';
+  route: string = '/posts/example';
+  id: string[] = ['jfer8iqw9fjqefj8q4j8q4jfqj', 'fdas7f8dasf9sa87sda9f789sa', '7fdsaf9d7g8f9h7g9hf0h', 'yt8re9jbyb09rtby0rt8by0r']
+  posts: any[] = [
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 1'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 2'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 4'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 5'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 6'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 7'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 8'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 9'},
+    { id: this.id[Math.floor(Math.random() * 4)], title: 'Post Title 10'},
+];
+}
