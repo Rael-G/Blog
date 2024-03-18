22e2492198c8967742c62ba20b7ddc250bb82bb8
Israel Albino Galvan <israel.galvan@live.com>
2024-03-18 13:40:54 -0300

    Home lists real posts

--- src/Blog.WebUi/src/app/components/pages/home/home.component.ts
+++ src/Blog.WebUi/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { PaginationComponent } from "../../pagination/pagination.component";
 import { RouterLink } from '@angular/router';
+import { PostService } from '../../../services/post/post.service';
+import { Post } from '../../../models/Post';
+import { enviroment } from '../../../../enviroments/enviroment';
 
 @Component({
   selector: 'app-home',
@@ -11,30 +14,26 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent {
 
-  currentPage: number = 1;
-  pageSize: number = 10;
-  totalPosts: number = 100;
+  protected currentPage: number = 1;
+  protected pageSize: number = 10;
+  protected totalPosts: number = 100;
+  protected posts: Post[] = []
+  protected categoriesUrl: string = enviroment.baseAppUrl + '/categories'
+  private postService: PostService
 
-  constructor() { }
+  constructor(postService: PostService) {
+    this.postService = postService 
+   }
+
+  ngOnInit() : void{
+    this.postService.getPosts().subscribe((posts) => {
+      this.posts = posts
+    });
+  }
 
   onPageChange(pageNumber: number) {
     this.currentPage = pageNumber;
     //Logic to call posts
   }
 
-  //examples
-  lorem: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, enim vel sodales volutpat, lorem ipsum aliquet velit, ut hendrerit nulla odio ut dolor. Quisque vulputate risus id lacus commodo, et rutrum arcu lacinia. Phasellus hendrerit diam at leo congue sollicitudin.';
-  route: string = '/posts/example';
-  categories: string[] = ['physic', 'alchemy', 'biology', 'matematical'];
-  posts: any[] = [
-    { title: 'Post Title 1', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 2', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 4', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 5', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 6', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 7', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 8', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 9', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-    { title: 'Post Title 10', summary: this.lorem, route: this.route, categorie: this.categories[Math.floor(Math.random() * 4)] },
-  ];
 }
