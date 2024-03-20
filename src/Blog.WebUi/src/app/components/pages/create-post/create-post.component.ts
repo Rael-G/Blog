fe1a4160cac356fe3626a7c053d78b8b650d239f
Israel Albino Galvan <israel.galvan@live.com>
2024-03-20 13:04:17 -0300

    Implement Separation between create-post and post-form

--- src/Blog.WebUi/src/app/components/pages/create-post/create-post.component.ts
+++ src/Blog.WebUi/src/app/components/pages/create-post/create-post.component.ts
@@ -1,63 +1,28 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Post } from '../../../models/Post';
 import { PostService } from '../../../services/post/post.service';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { PostFormComponent } from '../../post-form/post-form.component';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [PostFormComponent],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.scss'
 })
 export class CreatePostComponent {
 
-  protected postForm: FormGroup
-  protected submitted = false
 
-  private postService: PostService
-  private router: Router
-
-  constructor(postService: PostService, router: Router) {
-    this.postForm = new FormGroup({
-      title: new FormControl('', [Validators.required, Validators.maxLength(255)]),
-      content: new FormControl('', [Validators.required]),
-      category: new FormControl('', [Validators.required])
-    });
-
-    this.postService = postService
-    this.router = router
-
-  }
+  constructor(private postService: PostService, private router: Router) { }
 
   createHandler(post: Post) {
     this.postService.createPost(post).subscribe()
-
     this.redirect()
   }
 
-  submit() {
-    this.submitted = true
-
-    if (this.postForm.invalid)
-      return;
-
-    this.createHandler(this.postForm.value)
-  }
-
   redirect(){
-    this.router.navigate(['/'])
+    this.router.navigate(['/management'])
   }
 
-
-
-  //example
-  categories: string[] = [
-    'Sports',
-    'Tecnology',
-    'Home',
-    'Life'
-  ]
 }
---
+++ src/Blog.WebUi/src/app/components/post-form/post-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Post } from '../../models/Post';
+
+@Component({
+  selector: 'app-post-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './post-form.component.html',
+  styleUrl: './post-form.component.scss'
+})
+export class PostFormComponent implements OnInit {
+
+  protected postForm: FormGroup
+  protected submitted = false
+  @Input() post!: Post;
+
+  @Output() postSubmitted = new EventEmitter<Post>();
+  
+  constructor() {
+    this.postForm = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      content: new FormControl('', [Validators.required]),
+      category: new FormControl('', [Validators.required])
+    });
+  }
+
+  ngOnInit(){
+    this.getPostDetails();
+  }
+
+  getPostDetails() {
+      this.postForm.patchValue({
+        title: this.post?.title,
+        content: this.post?.content
+      });
+  }
+
+  submit() {
+    this.submitted = true
+
+    if (this.postForm.invalid)
+      return
+
+    this.postSubmitted.emit(this.postForm.value)
+  }
+  
+  //example
+  categories: string[] = [
+    'Sports',
+    'Tecnology',
+    'Home',
+    'Life'
+  ]
+}
