6682df23139be2766155577b514ac8d376b3b80d
Israel Albino Galvan <israel.galvan@live.com>
2024-04-01 19:27:22 -0300

    Feat Context and Migration for Tag

--- src/Blog.Persistance/Contexts/ApplicationDbContext.cs
+++ src/Blog.Persistance/Contexts/ApplicationDbContext.cs
@@ -13,23 +13,36 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     {
         base.OnModelCreating(modelBuilder);
 
+        ConfigureBaseEntity<Post>(modelBuilder);
         modelBuilder.Entity<Post>(post =>
         {
-            post.HasKey(p => p.Id);
-            post.Property(p => p.CreatedTime).IsRequired().ValueGeneratedOnAdd();
-            post.Property(p => p.ModifiedTime).IsRequired();
             post.Property(p => p.Title).IsRequired().HasMaxLength(256);
             post.Property(p => p.Content).IsRequired();
+            post.HasIndex(p => p.Title).IsUnique();
         });
 
+        ConfigureBaseEntity<Comment>(modelBuilder);
         modelBuilder.Entity<Comment>(comment =>
         {
-            comment.HasKey(c => c.Id);
-            comment.Property(c => c.CreatedTime).IsRequired().ValueGeneratedOnAdd();
-            comment.Property(c => c.ModifiedTime).IsRequired();
             comment.Property(c => c.Author).IsRequired().HasMaxLength(256);
             comment.Property(c => c.Content).IsRequired().HasMaxLength(512);
             comment.HasOne(c => c.Post).WithMany(p => p.Comments).HasForeignKey(c => c.PostId).IsRequired();
+        });
+
+        ConfigureBaseEntity<Tag>(modelBuilder);
+        modelBuilder.Entity<Tag>(tag => 
+        {
+            tag.Property(t => t.Name).IsRequired();
+            tag.HasIndex(t => t.Name).IsUnique();
+        });
+    }
+
+    private void ConfigureBaseEntity<T>(ModelBuilder modelBuilder) where T : BaseEntity
+    {
+        modelBuilder.Entity<T>(entity => {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.CreatedTime).IsRequired().ValueGeneratedOnAdd();
+            entity.Property(e=> e.ModifiedTime).IsRequired();
         });
     }
 }
---
+++ src/Blog.Persistance/Migrations/20240401222456_AddTag.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostTag",
+                columns: table => new
+                {
+                    PostsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostTag", x => new { x.PostsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_PostTag_Posts_PostsId",
+                        column: x => x.PostsId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostTag_Tag_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Title",
+                table: "Posts",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostTag_TagsId",
+                table: "PostTag",
+                column: "TagsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_Name",
+                table: "Tag",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostTag");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Title",
+                table: "Posts");
+        }
+    }
+}
