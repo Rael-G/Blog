77b2e8c0b8569133288c3de6400559682dea5656
Israel Albino Galvan <israel.galvan@live.com>
2024-04-03 16:30:47 -0300

    Fix TagDto properties

--- src/Blog.Application/Dtos/TagDto.cs
+++ src/Blog.Application/Dtos/TagDto.cs
@@ -2,11 +2,17 @@
 
 public class TagDto
 {
+    public Guid Id { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public DateTime ModifiedTime { get; set; }
     public string Name { get; set; }
     public IEnumerable<PostDto> Posts { get; set; }
 
-    public TagDto(string name, IEnumerable<PostDto> posts)
+    public TagDto(Guid id, DateTime createdTime, DateTime modifiedTime, string name, IEnumerable<PostDto> posts)
     {
+        Id = id;
+        CreatedTime = createdTime;
+        ModifiedTime = modifiedTime;
         Name = name;
         Posts = posts;
     }
