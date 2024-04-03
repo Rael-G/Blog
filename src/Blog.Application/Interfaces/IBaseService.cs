a8ae90054800daf673f68c384717badc1783a75c
Israel Albino Galvan <israel.galvan@live.com>
2024-04-03 15:30:59 -0300

    Feat BaseService to reduce redundance

---
+++ src/Blog.Application/Interfaces/IBaseService.cs
@@ -0,0 +1,40 @@
+﻿namespace Blog.Application;
+
+public interface IBaseService<TDto> 
+{
+    /// <summary>
+    /// Creates a new <see cref="TDto"/>.
+    /// </summary>
+    /// <param name="entity">The <see cref="TDto"/> to create.</param>
+    void Create(TDto entity);
+
+    /// <summary>
+    /// Updates an existing <see cref="TDto"/>.
+    /// </summary>
+    /// <param name="entity">The <see cref="TDto"/> to update.</param>
+    void Update(TDto entity);
+
+    /// <summary>
+    /// Deletes an existing <see cref="TDto"/>.
+    /// </summary>
+    /// <param name="entity">The <see cref="TDto"/> to delete.</param>
+    void Delete(TDto entity);
+
+    /// <summary>
+    /// Retrieves a <see cref="TDto"/> by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the <see cref="TDto"/> to retrieve.</param>
+    /// <returns>The retrieved <see cref="TDto"/>, or null if not found.</returns>
+    Task<TDto?> Get(Guid id);
+
+    /// <summary>
+    /// Retrieves all <see cref="TDto"/>s.
+    /// </summary>
+    /// <returns>A collection of <see cref="TDto"/>.</returns>
+    Task<IEnumerable<TDto>> GetAll();
+
+    /// <summary>
+    /// Save Changes
+    /// </summary>
+    Task Commit();
+}
---
+++ src/Blog.Application/Services/BaseService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Blog.Domain;
+
+namespace Blog.Application;
+
+public class BaseService<TDto, TEntity>
+    (IBaseRepository<TEntity> repository, IMapper mapper)
+    : IBaseService<TDto> 
+    where TEntity : BaseEntity
+{
+    private readonly IBaseRepository<TEntity> _repository = repository;
+    private readonly IMapper _mapper = mapper;
+
+    public void Create(TDto dto)
+    {
+        var entity = _mapper.Map<TEntity>(dto);
+        _repository.Create(entity);
+    }
+
+    public void Update(TDto dto)
+    {
+        var entity = _mapper.Map<TEntity>(dto);
+        _repository.Update(entity);
+    }
+
+    public void Delete(TDto dto)
+    {
+        var entity = _mapper.Map<TEntity>(dto);
+        _repository.Delete(entity);
+    }
+
+    public async Task<TDto?> Get(Guid id)
+    {
+        var entity = await _repository.Get(id);
+        return _mapper.Map<TDto>(entity);
+    }
+
+    public async Task<IEnumerable<TDto>> GetAll()
+    {
+        var entity = await _repository.GetAll();
+        return _mapper.Map<IEnumerable<TDto>>(entity);
+    }
+
+    public async Task Commit()
+    {
+        await _repository.Commit();
+    }
+}
+
+
