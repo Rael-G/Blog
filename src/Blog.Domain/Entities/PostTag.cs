691a1049fb485f380778328af5db66fa0aa67baa
Israel Albino Galvan <israel.galvan@live.com>
2024-04-06 23:15:35 -0300

    Feat validate ids and fix doc

--- src/Blog.Domain/Entities/PostTag.cs
+++ src/Blog.Domain/Entities/PostTag.cs
@@ -6,33 +6,47 @@
 public class PostTag
 {
     /// <summary>
-    /// Gets or sets the ID of the associated post.
+    /// Gets the ID of the associated post.
     /// </summary>
-    public Guid PostId { get; }
+    /// <exception cref="ArgumentNullException">Thrown when <see cref="PostId"/> is null or empty.</exception>
+    public Guid PostId { get => _postId; private set => _postId = ValidateId(value); }
 
     /// <summary>
-    /// Gets or sets the ID of the associated tag.
+    /// Gets the ID of the associated tag.
     /// </summary>
-    public Guid TagId { get; }
+    /// <exception cref="ArgumentNullException">Thrown when <see cref="TagId"/> is null or empty.</exception>
+    public Guid TagId { get => _tagId; private set => _tagId = ValidateId(value); }
 
     /// <summary>
-    /// Gets or sets the associated Post object.
+    /// Gets the associated Post object.
     /// </summary>
     public Post? Post { get; }
 
     /// <summary>
-    /// Gets or sets the associated Tag object.
+    /// Gets the associated Tag object.
     /// </summary>
     public Tag? Tag { get; }
+
+    private Guid _postId = Guid.Empty;
+    private Guid _tagId = Guid.Empty;
 
     /// <summary>
     /// Creates a new instance of the PostTag class with the specified IDs.
     /// </summary>
     /// <param name="postId">The ID of the post.</param>
     /// <param name="tagId">The ID of the tag.</param>
+    /// <exception cref="ArgumentNullException"></exception>
     public PostTag(Guid postId, Guid tagId)
     {
         PostId = postId;
         TagId = tagId;
+    }
+
+    private static Guid ValidateId(Guid id)
+    {
+        if (id == Guid.Empty)
+            throw new ArgumentNullException(nameof(id));
+
+        return id;
     }
 }
