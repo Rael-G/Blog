6a51ab52cf87000efaf4d703d31a6e029fadca29
Raelg-G <israel.galvan@live.com>
2024-04-10 14:40:21 -0300

    Feat add association of post and tags for post-form component

--- src/Blog.WebUi/src/app/components/post-form/post-form.component.ts
+++ src/Blog.WebUi/src/app/components/post-form/post-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Post } from '../../interfaces/Post';
+import { TagService } from '../../services/tag/tag.service';
+import { Tag } from '../../interfaces/Tag';
 
 @Component({
   selector: 'app-post-form',
@@ -12,43 +14,65 @@ import { Post } from '../../interfaces/Post';
 export class PostFormComponent implements OnInit {
   protected postForm: FormGroup
   protected submitted = false
+  protected tags: Tag[] = []
   @Input() post!: Post
 
   @Output() postSubmitted = new EventEmitter<Post>()
 
-  constructor() {
+  constructor(private tagService : TagService) {
     this.postForm = new FormGroup({
       title: new FormControl('', [Validators.required, Validators.maxLength(255)]),
       content: new FormControl('', [Validators.required]),
-      category: new FormControl('', [Validators.required])
+      tags: new FormControl('', [Validators.required])
     });
   }
 
   ngOnInit() {
     this.getPostDetails()
+    this.loadTag();
   }
 
-  getPostDetails() {
+  private getPostDetails() {
+    let tagIds : string[] = []
+
+    for(let tag of this.post?.tags){
+      if (tag.id)
+        tagIds.push(tag.id)
+    }
+
     this.postForm.patchValue({
       title: this.post?.title,
-      content: this.post?.content
+      content: this.post?.content,
+      tags: tagIds
     })
   }
 
-  submit() {
+  private loadTag(){
+    this.tagService.getTags().subscribe(tags => 
+      this.tags = tags
+    )
+  }
+
+  private mapTags()
+  {
+    const ids = this.postForm.get('tags')?.value as string[]
+    let mappedTags : Tag[] = []
+    for(let index = 0; index < ids.length; index++){
+      let tag = this.tags.find(t => t.id == ids[index])
+      if (tag)
+        mappedTags.push(tag);
+    }
+    this.postForm.patchValue({ tags: mappedTags });
+  }
+
+  protected submit() {
     this.submitted = true
 
     if (this.postForm.invalid)
       return
 
+    this.mapTags();
+    
     this.postSubmitted.emit(this.postForm.value)
   }
-
-  //example
-  categories: string[] = [
-    'Sports',
-    'Tecnology',
-    'Home',
-    'Life'
-  ]
 }
