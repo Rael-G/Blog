657eed823681a49c112b20ef2f4f5abdad00da32
Raelg-G <israel.galvan@live.com>
2024-04-25 16:44:32 -0300

    Style fix identation

--- src/Blog.Persistance/Contexts/ApplicationDbContext.cs
+++ src/Blog.Persistance/Contexts/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Blog.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Expressions.Internal;
 
 namespace Blog.Persistance;
 
@@ -20,32 +19,49 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         ConfigureBaseEntity<Post>(modelBuilder);
         modelBuilder.Entity<Post>(post =>
         {
-            post.Property(p => p.Title).IsRequired().HasMaxLength(256);
-            post.Property(p => p.Content).IsRequired();
-            post.HasIndex(p => p.Title).IsUnique();
+            post.Property(p => p.Title)
+                .IsRequired()
+                .HasMaxLength(256);
+            post.Property(p => p.Content)
+                .IsRequired();
+            post.HasIndex(p => p.Title)
+                .IsUnique();
             post.HasIndex(p => p.CreatedTime);
         });
 
         ConfigureBaseEntity<Comment>(modelBuilder);
         modelBuilder.Entity<Comment>(comment =>
         {
-            comment.Property(c => c.Author).IsRequired().HasMaxLength(256);
-            comment.Property(c => c.Content).IsRequired().HasMaxLength(512);
-            comment.HasOne(c => c.Post).WithMany(p => p.Comments).HasForeignKey(c => c.PostId).IsRequired();
+            comment.Property(c => c.Author)
+                .IsRequired()
+                .HasMaxLength(256);
+            comment.Property(c => c.Content)
+                .IsRequired()
+                .HasMaxLength(512);
+            comment.HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .IsRequired();
         });
 
         ConfigureBaseEntity<Tag>(modelBuilder);
         modelBuilder.Entity<Tag>(tag =>
         {
-            tag.Property(t => t.Name).IsRequired();
-            tag.HasIndex(t => t.Name).IsUnique();
+            tag.Property(t => t.Name)
+                .IsRequired();
+            tag.HasIndex(t => t.Name)
+                .IsUnique();
         });
 
         modelBuilder.Entity<PostTag>(postTag =>
         {
             postTag.HasKey(pt => new { pt.PostId, pt.TagId });
-            postTag.HasOne(pt => pt.Post).WithMany(p => p.Tags).HasForeignKey(pt => pt.PostId);
-            postTag.HasOne(pt => pt.Tag).WithMany(t => t.Posts).HasForeignKey(pt => pt.TagId);
+            postTag.HasOne(pt => pt.Post)
+                .WithMany(p => p.Tags)
+                .HasForeignKey(pt => pt.PostId);
+            postTag.HasOne(pt => pt.Tag)
+                .WithMany(t => t.Posts)
+                .HasForeignKey(pt => pt.TagId);
         });
             
     }
@@ -55,8 +71,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.Entity<T>(entity =>
         {
             entity.HasKey(e => e.Id);
-            entity.Property(e => e.CreatedTime).IsRequired().Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
-            entity.Property(e => e.ModifiedTime).IsRequired();
+            entity.Property(e => e.CreatedTime)
+                .IsRequired()
+                .Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
+            entity.Property(e => e.ModifiedTime)
+                .IsRequired();
         });
     }
 }
