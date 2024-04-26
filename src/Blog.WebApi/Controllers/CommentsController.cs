81d77c535d8917a06934c8aa72ab67cbebebb9e7
Raelg-G <israel.galvan@live.com>
2024-04-26 17:37:23 -0300

    Fix remove unnecessary endpoint

--- src/Blog.WebApi/Controllers/CommentsController.cs
+++ src/Blog.WebApi/Controllers/CommentsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Blog.Application;
 using Blog.WebApi.Models.Input;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Blog.WebApi.Controllers;
 
@@ -15,6 +16,7 @@ public class CommentsController(ICommentService commentsService, IPostService po
     /// </summary>
     /// <param name="postId">The ID of the post.</param>
     /// <returns>Returns a list of comments for the specified post.</returns>
+    [AllowAnonymous]
     [HttpGet]
     [ProducesResponseType(200)] // OK
     [ProducesResponseType(404)] // Not Found
@@ -32,6 +34,7 @@ public class CommentsController(ICommentService commentsService, IPostService po
     /// </summary>
     /// <param name="id">The ID of the comment to retrieve.</param>
     /// <returns>Returns the comment if found, otherwise returns a 404 Not Found.</returns>
+    [AllowAnonymous]
     [HttpGet("{id}")]
     [ProducesResponseType(200)] // OK
     [ProducesResponseType(404)] // Not Found
@@ -43,6 +46,7 @@ public class CommentsController(ICommentService commentsService, IPostService po
     /// </summary>
     /// <param name="input">The input model containing data for the new comment.</param>
     /// <returns>Returns the newly created comment.</returns>
+    [AllowAnonymous]
     [HttpPost]
     [ProducesResponseType(201)] // Created
     [ProducesResponseType(400)] // Bad Request
@@ -60,23 +64,11 @@ public class CommentsController(ICommentService commentsService, IPostService po
     }
 
     /// <summary>
-    /// Updates an existing comment.
-    /// </summary>
-    /// <param name="id">The ID of the comment to update.</param>
-    /// <param name="input">The input model containing updated data for the comment.</param>
-    /// <returns>Returns 204 No Content if successful, otherwise returns a 400 Bad Request or 404 Not Found.</returns>
-    [HttpPut("{id}")]
-    [ProducesResponseType(204)] // No Content
-    [ProducesResponseType(400)] // Bad Request
-    [ProducesResponseType(404)] // Not Found
-    public async Task<IActionResult> Put(Guid id, [FromBody] CommentInputModel input)
-        => await base.Put(id, input);
-
-    /// <summary>
     /// Deletes a comment by its ID.
     /// </summary>
     /// <param name="id">The ID of the comment to delete.</param>
     /// <returns>Returns 204 No Content if successful, otherwise returns a 404 Not Found.</returns>
+    [Authorize(Roles = Roles.Moderator)]
     [HttpDelete("{id}")]
     [ProducesResponseType(204)] // No Content
     [ProducesResponseType(404)] // Not Found
