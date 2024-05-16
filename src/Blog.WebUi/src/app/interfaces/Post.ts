9ff04f57111a044e41bd7f1e89e388bc10292c6b
Raelg-G <israel.galvan@live.com>
2024-05-16 16:49:24 -0300

    Refactor user for post and roles for user

--- src/Blog.WebUi/src/app/interfaces/Post.ts
+++ src/Blog.WebUi/src/app/interfaces/Post.ts
@@ -1,10 +1,12 @@
 import { Comment } from "./Comment";
 import { Tag } from "./Tag";
+import { User } from "./User";
 
 export interface Post {
     id?: string,
     title: string,
     content: string,
     comments: Comment[]
-    tags: Tag[]
+    tags: Tag[],
+    user : User
 }
--- src/Blog.WebUi/src/app/interfaces/User.ts
+++ src/Blog.WebUi/src/app/interfaces/User.ts
@@ -1,7 +1,8 @@
 import { Post } from "./Post";
 
 export interface User {
-    id : string,
-    userName : string,
+    id : string
+    userName : string
     posts : Post[]
+    roles : string[]
 }
