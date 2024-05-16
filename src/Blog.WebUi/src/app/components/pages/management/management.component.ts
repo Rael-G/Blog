86ff321579962e79c604e862edd4548c62d7020a
Raelg-G <israel.galvan@live.com>
2024-05-16 16:47:15 -0300

    Refactor moved posts management logic to posts-management component

--- src/Blog.WebUi/src/app/components/pages/management/management.component.ts
+++ src/Blog.WebUi/src/app/components/pages/management/management.component.ts
@@ -1,59 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { PostService } from '../../../services/post/post.service';
-import { Post } from '../../../interfaces/Post';
-import { CommentService } from '../../../services/comment/comment.service';
+import { Component } from '@angular/core';
 import { CreateTagComponent } from '../../create-tag/create-tag.component';
-import { TablePostComponent } from '../../table-post/table-post.component';
-import { PaginationComponent } from '../../pagination/pagination.component';
-import { AuthService } from '../../../services/auth/auth.service';
+import { PostsManagementComponent } from '../../posts-management/posts-management.component';
 
 @Component({
   selector: 'app-management',
   standalone: true,
-  imports: [RouterLink, CreateTagComponent, TablePostComponent, PaginationComponent],
+  imports: [PostsManagementComponent, CreateTagComponent],
   templateUrl: './management.component.html',
   styleUrl: './management.component.scss'
 })
-export class ManagementComponent implements OnInit {
-  protected posts: Post[] = []
-  protected currentPage: number = 1
-  protected totalPages: number = 0
-  protected pageNumbers : number[] = []
+export class ManagementComponent {
   
-  constructor(private postService: PostService, private commentService : CommentService, private authService: AuthService, private router: Router) { }
-
-  ngOnInit(): void {
-    this.getPosts()
-  }
-
-  protected onPageChange(pageNumber: number) {
-    this.posts = []
-    this.postService.getPosts(pageNumber).subscribe((posts) => 
-      this.posts = posts)
-    this.currentPage = pageNumber
-    this.loadPageCount()
-  }
-
-  protected loadPageCount(){
-    this.postService.getPageCount().subscribe((count) => {
-      this.totalPages = count
-      this.pageNumbers = PaginationComponent.SetPageNumbers(this.currentPage, this.totalPages)
-    })
-  }
-
-  protected getPosts() {
-    this.postService.getPosts(this.currentPage)
-      .subscribe((posts) => {
-        this.posts = posts
-        for(let post of posts){
-          if (post.id){
-            this.commentService.getComments(post.id).subscribe((comments) => {
-              post.comments = comments
-            })
-          }
-        }
-        this.loadPageCount()
-      })
-  }
 }
