e2b03b01cc63d088097be38b347ebd8ff5bfa513
Raelg-G <israel.galvan@live.com>
2024-05-29 15:50:46 -0300

    Refactor remove about and contact page

--- src/Blog.WebUi/src/app/app.routes.ts
+++ src/Blog.WebUi/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { ContactComponent } from './components/pages/contact/contact.component';
 import { PostComponent } from './components/pages/post/post.component';
 import { ManagementComponent } from './components/pages/management/management.component';
 import { CreatePostComponent } from './components/pages/create-post/create-post.component';
@@ -11,8 +9,6 @@ import { TagComponent } from './components/pages/tag/tag.component';
 export const routes: Routes =
     [
         { path: '', component: HomeComponent },
-        { path: 'about', component: AboutComponent },
-        { path: 'contact', component: ContactComponent },
         { path: 'posts/example', component: PostComponent },
         { path: 'management', component: ManagementComponent },
         { path: 'create-post', component: CreatePostComponent },
