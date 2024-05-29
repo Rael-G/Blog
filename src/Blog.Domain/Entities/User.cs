d2fbee4fd53a7f5c0f2b035809a570e92dec570d
Raelg-G <israel.galvan@live.com>
2024-05-29 18:37:33 -0300

    Feat username validation

--- src/Blog.Domain/Entities/User.cs
+++ src/Blog.Domain/Entities/User.cs
@@ -1,7 +1,18 @@
-﻿namespace Blog.Domain;
+﻿using System.Text.RegularExpressions;
 
-public class User : BaseEntity
+namespace Blog.Domain;
+
+public partial class User : BaseEntity
 {
+    public string UserName { get => _userName; set => _userName = ValidateUsername(value); }
+    public string PasswordHash { get; set; }
+    public IEnumerable<string> Roles { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+    public IEnumerable<Post> Posts{ get; }
+
+    private string _userName = string.Empty;
+
     public User(Guid id, string userName, string passwordHash, IEnumerable<string>? roles = null) : base(id)
     {
         UserName = userName;
@@ -10,10 +21,14 @@ public class User : BaseEntity
         Posts = new List<Post>();
     }
 
-    public string UserName { get; set; }
-    public string PasswordHash { get; set; }
-    public IEnumerable<string> Roles { get; set; }
-    public string? RefreshToken { get; set; }
-    public DateTime? RefreshTokenExpiryTime { get; set; }
-    public IEnumerable<Post> Posts{ get; }
+    private static string ValidateUsername(string username)
+    {
+        if (!UserNameRegex().Match(username).Success)
+            throw new ArgumentException("Username must be alphanumeric and between 3 and 30 characters long");
+
+        return username;
+    }
+
+    [GeneratedRegex(@"^([a-zA-Z0-9-_]){3,30}$")]
+    private static partial Regex UserNameRegex();
 }
