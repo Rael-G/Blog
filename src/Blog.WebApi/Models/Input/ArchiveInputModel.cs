c322560656835d2604795361f8048afeb996cc11
Raelg-G <israel.galvan@live.com>
2024-07-01 16:14:43 -0300

    Feat archives controller, input and output models

---
+++ src/Blog.WebApi/Models/Input/ArchiveInputModel.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Application;
+
+namespace Blog.WebApi;
+
+public class ArchiveInputModel : IInputModel<ArchiveDto>
+{
+    public bool IsPublic { get; set; }
+
+    public ArchiveInputModel() { }
+
+    public ArchiveInputModel(bool isPublic)
+    {
+        IsPublic = isPublic;
+    }
+
+    public ArchiveDto InputToDto()
+        => new (){Id = Guid.NewGuid(), IsPublic = IsPublic};
+
+    public void InputToDto(ArchiveDto dto)
+    {
+        dto.IsPublic = IsPublic;
+    }
+}
---
+++ src/Blog.WebApi/Models/Output/ArchiveOutputModel.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Application;
+
+namespace Blog.WebApi;
+
+public class ArchiveOutputModel
+{
+    public Guid Id { get; set; }
+
+    public string FileName { get; set; } = string.Empty;
+
+    public string ContentUrl { get; set; } = string.Empty;
+
+    public string ContentType { get; set; } = string.Empty;
+
+    public Guid OwnerId { get; set; }
+
+    public bool IsPublic { get; set; }
+
+    public ArchiveOutputModel(){ }
+
+    public ArchiveOutputModel(ArchiveDto archive)
+    {
+        Id = archive.Id;
+        FileName = archive.FileName;
+        ContentUrl = archive.ContentUrl;
+        ContentType = archive.ContentType;
+        OwnerId = archive.OwnerId;
+        IsPublic = archive.IsPublic;
+    }
+
+    public static IEnumerable<ArchiveOutputModel> MapRange(IEnumerable<ArchiveDto> archiveDtos)
+    {
+        List<ArchiveOutputModel> archiveOutputs = [];
+        foreach (var archiveDto in archiveDtos)
+        {
+            archiveOutputs.Add(new ArchiveOutputModel(archiveDto));
+        }
+
+        return archiveOutputs;
+    }
+}
