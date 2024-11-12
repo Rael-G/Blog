df8cfa98a5d0b109f848c0fa4918a42b892195c0
Raelg-G <israel.galvan@live.com>
2024-11-11 21:27:14 -0300

    Refactor remove archive table migration

---
+++ src/Blog.Persistance/Migrations/20241111223857_RemoveArchive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveArchive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Archive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Archive",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContentType = table.Column<string>(type: "text", nullable: false),
+                    ContentUrl = table.Column<string>(type: "text", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    IsPublic = table.Column<bool>(type: "boolean", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Archive", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Archive_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Archive_OwnerId",
+                table: "Archive",
+                column: "OwnerId");
+        }
+    }
+}
